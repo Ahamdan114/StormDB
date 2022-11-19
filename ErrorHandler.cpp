176a19adaf21a4e6d6227eb60b79c0093b5a39fc
Ahmed-Iulian Hamdan <hamdan.ahmed002@gmail.com>
2022-11-19 16:07:48 +0200

    Added imports.
    Making imports for project functionality.

--- ErrorHandler.cpp
+++ ErrorHandler.cpp
@@ -1,9 +1,20 @@
+#pragma once
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 class ErrorHandler {
+	int age = 0;
+public:
+	ErrorHandler(int age) {
+		this->age = age;
+	}
+
+	int getAge() {
+		return age;
+	}
+
 	~ErrorHandler() {
 		cout << "Destructor called" << endl;
 	}
--- Root.cpp
+++ Root.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <string>
 
+#include "ErrorHandler.cpp";
+#include "Print.cpp";
+#include "ParsingChecker.cpp";
+#include "LogicChecker.cpp";
+#include "FileHandler.cpp";
+
 using namespace std;
 
 int main() {
-	string input;
+	//string input;
 
-	while (true) {
-		getline(cin, input);
-	}
+	//while (true) {
+	//	getline(cin, input);
+	//}
+	ErrorHandler handle = ErrorHandler(27);
+	cout << handle.getAge() << endl;
 }
