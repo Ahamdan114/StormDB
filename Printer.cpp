d027e931d9d87d7ac4972ba1d5b9aae695b0bfda
AndreiEnculescu69 <andreienculescu10@yahoo.com>
2023-01-05 21:40:37 +0200

    Added the "Help Manual"

--- Printer.cpp
+++ Printer.cpp
@@ -8,6 +8,80 @@ public:
 		cout << "Command details -> The command introduced is: " << cleanInput << endl;
 	}
 
+	void returnManual(string word) {
+		// (create|drop|display|insert|select|delete|update|import)
+		cout << "The word is: " << word << endl;
+
+		if (word == "create") {
+			cout << endl << endl;
+			cout << "The CREATE statement is used to create database objects such as tables, views, stored procedures, and other objects in an SQL database." << endl;
+			cout << "General syntax :" << endl;
+			cout << "create table table_name(var_name, integer / float / text, size, default_value)																		-> For a table with one column" << endl;
+			cout << "create table table_name((var_name1, integer / float / text, size1, default_value1), (var_name2, integer / float / text, size2, default_value2))	-> For a table with two columns" << endl;
+			cout << endl << endl;
+		}
+
+		else if (word == "drop") {
+			cout << endl << endl;
+			cout << "The DROP statement is used to delete database objects such as tables, views, stored procedures, and other objects in an SQL database." << endl;
+			cout << "General syntax :" << endl;
+			cout << "drop table table_name" << endl;
+			cout << endl << endl;
+		}
+
+		else if (word == "display") {
+			cout << endl << endl;
+			cout << "The DISPLAY statement is used to display a database object brougth to life by the CREATE statement." << endl;
+			cout << "General syntax :" << endl;
+			cout << "display table table_name" << endl;
+			cout << endl << endl;
+		}
+
+		else if (word == "insert") {
+			cout << endl << endl;
+			cout << "The INSERT statement is used to add new records to a database table." << endl;
+			cout << "General syntax :" << endl;
+			cout << "insert into table_name values(value1, value2, ..., valueN)" << endl;
+			cout << "Rules :" << endl;
+			cout << "The values need to be the exact numberand have the exact order like they are in the original table.(You can use Display to check your original table values)" << endl;
+			cout << endl << endl;
+		}
+
+		else if (word == "select") {
+			cout << endl << endl;
+			cout << "The SELECT statement is used to retrieve data from a database table." << endl;
+			cout << "General syntax :" << endl;
+			cout << "select(column1, column2, ..., columnN) from table_name							-> specific columns" << endl;
+			cout << "select(column1, column2, ..., columnN) from table_name where var_name = filter -> select with a filter" << endl;
+			cout << "select all from table_name														-> select all columns" << endl;
+			cout << endl << endl;
+		}
+
+		else if (word == "delete") {
+			cout << endl << endl;
+			cout << "The DELETE statement is used to delete records from a database table." << endl;
+			cout << "General syntax :" << endl;
+			cout << "delete from table_name where var_name = value" << endl;
+			cout << endl << endl;
+		}
+
+		else if (word == "update") {
+			cout << endl << endl;
+			cout << "The UPDATE statement is used to modify existing records in a database table." << endl;
+			cout << "General syntax :" << endl;
+			cout << "update table_name set var_name1 = value1 where var_name2 = value2" << endl;
+			cout << endl << endl;
+		}
+
+		else if (word == "import") {
+			cout << endl << endl;
+			cout << "The IMPORT statement is used to import data from a file into a database table." << endl;
+			cout << "General syntax :" << endl;
+			cout << "import table_name file_name.csv" << endl;
+			cout << endl << endl;
+		}
+	}
+
 	// The method if the command is valid displays the succes message
 	void returnStatement(int number) {
 		/*switch (number) {*/
@@ -35,7 +109,7 @@ public:
 		default:
 			cout << "ERROR: NO COMMAND ACCEPTED." << endl;
 			*/
-		/*}*/
+			/*}*/
 	}
 
 	// The method returns a goodbye message at the end of the program
